070d18fbd9df1ec3956440b03178e9f48f260b52
Manoj Paikrao <paikraomanoj358@gmail.com>
2022-12-24 18:57:00 +0530

    added LinkedList code

---
+++ src/com/velocity/demo/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.velocity.demo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		System.out.println(list);
+	}
+
+}
